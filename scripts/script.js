c844486b01029377cd8d4b6d8e13df324715f63c
JVSC99 <jvsc76@gmail.com>
2022-03-03 14:40:16 -0300

    primeira versão

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let login = document.getElementById("login")
+let password = document.getElementById("password")
+let buttonLogin = document.getElementById("button-login")
+let body = document.getElementById("body")
+let containerLogin = document.getElementById('container-login')
+
+document.addEventListener("keypress", function(e) {
+    if(e.key === 'Enter') {
+      buttonLogin.click();
+    }
+});
+
+function Logar(){
+
+    if((login.value === "user@gmail.com") && (password.value === "1234")){
+        alert('logado')
+        containerLogin.innerHTML = "<div id='load'> <div id='loading'></div></div>"; 
+        setTimeout(function(){
+            renderMenu();
+        },2000)
+    }else{
+        alert("Login ou senha incorreto")
+    }
+}
+
+
+function renderMenu(){
+    containerLogin.innerHTML = ""
+    body.style.backgroundImage = 'linear-gradient(gray,gray)'
+    
+}
